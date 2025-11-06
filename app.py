2d81f1008397fd255c079de9c6cd06209d1c0b04
alinac1888 <alinac44@nycstudents.net>
2025-11-06 06:25:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ data = json.load(movies)
 for index, movie in enumerate(data):
     print(index, ":",(movie)['title'])
 
+# year after and before
 # choice = int(input("year after"))
 # choicee = int(input("year before"))
 
@@ -12,12 +13,19 @@ for index, movie in enumerate(data):
 #     if choice < movie['year'] < choicee:
 #         print(movie['title'], movie['year'])
 
-choiceee = int(input("during year"))
-for movie in data:
-    if choiceee == movie['year']:
-        print(movie['title'])
+# during year
+# choiceee = int(input("during year"))
+# for movie in data:
+#     if choiceee == movie['year']:
+#         print(movie['title'])
 
-specific = input("EXACT movie u like")
-while True:
-    if specific == movie['title']:
-        print(movie['title'])
+for i in data:  
+    specific = input("EXACT movie u like")
+    booth = []
+    booth.append(specific)
+    booth.append(movie['title'])
+    if len(specific) == len(movie['title']):
+        print(f"{movie['title']} is avliable")
+    else: 
+        print(f"{specific} is not found")
+        break 
